e3c18b4ac90f55d341f2ec0faaac76acefcd1ff8
Mathivanan <118725547+mathiappu@users.noreply.github.com>
2022-11-21 15:29:31 +0530

    Update streamlit_app.py
    fixed character size
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import streamlit
 streamlit.title('My Parents Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.title('Omega 3 & Blueberry Oatmeal')
-streamlit.title('Kale, Spinach & Rocket Smoothie')
-streamit.title('Hard-Boiled Free-Range Egg')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
